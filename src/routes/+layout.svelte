8128d129a4636d2dbbc99052be1f80041b8a9498
Pete McPherson <pnm326@gmail.com>
2024-03-19 20:39:39 -0400

    added tailwind and css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    import "../app.css";
+
+    export let data;
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Template Jam!</h1>
+<p>This site is currently under construction ;)</p>
