440810b19f332777c94275ad836646fa97eafe69
Pete McPherson <pnm326@gmail.com>
2024-09-04 16:45:48 -0400

    adding content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to Template Jam!</h1>
-<p>This site is currently under construction ;)</p>
+<script>
+</script>
+
+<div class="bg-black">
+  <!-- top of page -->
+  <div class="w-3/4 mx-auto text-center py-12 md:py-32 space-y-10">
+    <h1 class="text-xl font-black">Template Jam - from Do You Even Blog</h1>
+    <h2
+      class="text-6xl font-black bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+    >
+      Helpful tools & templates you can use TODAY.
+    </h2>
+  </div>
+  <!-- features & embed -->
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mx-4 md:mx-24">
+    <!-- col 1 -->
+    <div class="col-span-2 space-y-4">
+      <h2 class="text-4xl font-black">ALL PETE'S TEMPLATES - JUST $99!</h2>
+      <p>
+        Want to produce content WAY faster? Or deploy a lightning-fast website
+        theme on Elementor (like, in 10 minutes?).
+      </p>
+      <p>
+        Want my funnel templates? Notion dashboard? Sales & nurture
+        sequences–completely with swipe file examples and tutorials for
+        everything?
+      </p>
+      <p>Want $1,700+ worth of tools for only $99?</p>
+      <h2>Template Jam includes:</h2>
+      <ul>
+        <li>🔥 NEW: Notion Weekly Planner</li>
+        <li>🔥 NEW: Niche Site Launch Checklist</li>
+        <li>🔥 NEW: Niche Site WordPress Theme templates</li>
+        <li>🔥 NEW: Niche Site KW research template & tracker</li>
+        <li>🔥 NEW: "LINK PAGES" tool for easy & fast internal links.</li>
+        <li>
+          🔥 NEW: 3 SEO blog post templates (info posts, reviews, round-up)
+        </li>
+        <li>✅ 5+ Notion templates</li>
+        <li>✅ 5+ SEO templates & tools</li>
+        <li>✅ 5+ Productivity templates</li>
+        <li>✅ 10+ Nurture & sales emails</li>
+        <li>✅ 5+ Podcast & YouTube templates</li>
+        <li>✅ 8+ Sales & Copywriting templates</li>
+        <li>✅ Sponsorship Agreement & Profit First Template</li>
+        <li>
+          ✅ 25+ Elementor templates (including this sales page & the
+          doyouevenblog.com theme)
+        </li>
+        <li>😎 All other templates I add in the future.</li>
+      </ul>
+    </div>
+    <!-- col 2 -->
+    <div>
+      <p>
+        Template Jam is currently closed. Join the waitlist below for unlimited
+        happiness (ok not really).
+      </p>
+      <form class="flex flex-col gap-4">
+        <div class="form-control w-full">
+          <label class="label" for="email">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="email" id="email" class="input input-bordered w-full" />
+        </div>
+        <button type="submit" class="btn btn-primary">Join Waitlist</button>
+      </form>
+    </div>
+  </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,43 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-		themes: ["forest", "dark", "cupcake", "cyberpunk"],
+		themes: [
+      // {
+			// 	acid: {
+			// 		...require('daisyui/src/theming/themes')['acid'],
+			// 		// neutral: '#0a002d'
+      //     neutral: '#090029'
+			// 	}
+			// },
+      "light",
+      "garden",
+      "forest",
+      "sunset",
+      "night",
+      "nord",
+// {
+//   night: {
+//     ...require('daisyui/src/theming/themes')['night'],
+//     // primary: '#F471B5',
+//     primary: '#05B6D3',
+//     neutral: '#153B50',
+//     "base-100": '#061E23'
+//   }
+// }  ,
+  //  {
+    //   sunset: {
+    //     ...require('daisyui/src/theming/themes')['sunset'],
+    //     primary: '#6200ee',
+    //   }
+    //  },
+      {
+        lofi: {
+          ...require('daisyui/src/theming/themes')['lofi'],
+          primary: '#6200ee',
+         
+        }
+      },
+      "lemonade", "cupcake", "acid"],
 	}
 }
 
