548b435d72704dd049a7c7c3622ac59a4e0a503e
Pete McPherson <pnm326@gmail.com>
2024-09-05 17:15:50 -0400

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+import liz_1 from "$lib/assets/images/liz_1.gif";
+import bmf from "$lib/assets/images/bmf.png";
 </script>
 
 <div class="bg-black">
@@ -6,7 +8,7 @@
   <div class="w-3/4 mx-auto text-center py-12 md:py-32 space-y-10">
     <h1 class="text-xl font-black">Template Jam - from Do You Even Blog</h1>
     <h2
-      class="text-6xl font-black bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+      class="text-5xl md:text-6xl font-black bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
     >
       Helpful tools & templates you can use TODAY.
     </h2>
@@ -15,7 +17,9 @@
   <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mx-4 md:mx-24">
     <!-- col 1 -->
     <div class="col-span-2 space-y-4">
-      <h2 class="text-4xl font-black">ALL PETE'S TEMPLATES - JUST $99!</h2>
+      <h2 class="text-2xl md:text-4xl font-black">
+        ALL PETE'S TEMPLATES - JUST $99!
+      </h2>
       <p>
         Want to produce content WAY faster? Or deploy a lightning-fast website
         theme on Elementor (like, in 10 minutes?).
@@ -25,17 +29,19 @@
         sequences–completely with swipe file examples and tutorials for
         everything?
       </p>
-      <p>Want $1,700+ worth of tools for only $99?</p>
-      <h2>Template Jam includes:</h2>
+      <p
+        class="font-bold bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+      >
+        Want $1,700+ worth of tools for only $99?
+      </p>
+      <h2 class="font-black text-2xl">Template Jam includes:</h2>
       <ul>
-        <li>🔥 NEW: Notion Weekly Planner</li>
-        <li>🔥 NEW: Niche Site Launch Checklist</li>
-        <li>🔥 NEW: Niche Site WordPress Theme templates</li>
-        <li>🔥 NEW: Niche Site KW research template & tracker</li>
-        <li>🔥 NEW: "LINK PAGES" tool for easy & fast internal links.</li>
-        <li>
-          🔥 NEW: 3 SEO blog post templates (info posts, reviews, round-up)
-        </li>
+        <li>✅ Notion Weekly Planner</li>
+        <li>✅ Niche Site Launch Checklist</li>
+        <li>✅ Niche Site WordPress Theme templates</li>
+        <li>✅ Niche Site KW research template & tracker</li>
+        <li>✅ "LINK PAGES" tool for easy & fast internal links.</li>
+        <li>✅ 3 SEO blog post templates (info posts, reviews, round-up)</li>
         <li>✅ 5+ Notion templates</li>
         <li>✅ 5+ SEO templates & tools</li>
         <li>✅ 5+ Productivity templates</li>
@@ -65,6 +71,85 @@
         </div>
         <button type="submit" class="btn btn-primary">Join Waitlist</button>
       </form>
+    </div>
+  </div>
+
+  <!-- but wait there's more -->
+  <div class="w-1/2 mx-auto text-center pt-4 md:pt-32">
+    <h2
+      class="text-2xl md:text-4xl md:text-7xl font-black bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+    >
+      BUT WAIT, THERE'S MORE!!
+    </h2>
+    <p class="text-xs my-4">I've always wanted to say that...</p>
+    <div class="my-24">
+      <h2 class="font-black text-4xl">
+        I'm also including a few bonuses, just for Liz's audience.
+      </h2>
+      <!-- features -->
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4 md:mt-24">
+        <div>
+          <h3
+            class="font-black mb-8 text-2xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+          >
+            1HR BLOGPOST <br /> ($27 value):
+          </h3>
+          <p>
+            You get complete access to the tool that has helped 2,500+ creators
+            create content FASTER
+          </p>
+        </div>
+        <div>
+          <h3
+            class="font-black mb-8 text-2xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+          >
+            LIGHTNING WRITING<br /> ($37 value):
+          </h3>
+          <p>
+            A mini-training where you'learn my 9-step system for creaune content
+          </p>
+        </div>
+        <div>
+          <h3
+            class="font-black mb-8 text-2xl bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+          >
+            CONTENT EVERYWHERE<br /> ($197 value):
+          </h3>
+          <p>
+            What about after you hit publish? You' get my full course on content
+            PROMOTION & RE-PURPOSING. Get more eyeballs with less work!
+          </p>
+        </div>
+      </div>
+      <div class="space-y-12">
+        <h2 class="font-black text-4xl text-center">
+          And oh yeah, I'm including my $3,997 high-ticket course on funnels.
+        </h2>
+        <img class="w-1/2 mx-auto" src={bmf} alt="bmf" />
+        <div class="text-xl text-left space-y-8">
+          <p>
+            B.M.F. <span class="italic">(Blog Monetization Funnels)</span> hasn't
+            been available to the public before (only to my membership students).
+          </p>
+          <p>
+            And no, I didn't just slap a $3,997 price tag on it to be fancy.
+          </p>
+          <p>
+            That's how much I sold it for, <span class="font-bold"
+              >and every single person who went through the program made that
+              investment back, AND THEN SOME.</span
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-1/4 mx-auto">
+    <div class="flex justify-center">
+      <img src={liz_1} alt="liz he's nuts" />
+    </div>
+    <div class="my-12">
+      <p>Nah Liz, I'm not. I'm just...well maybe a little.</p>
     </div>
   </div>
 </div>
