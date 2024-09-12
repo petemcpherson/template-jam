44e2d0dbe0c63f849416c055e194d9d634ec11ac
Pete McPherson <pnm326@gmail.com>
2024-09-12 11:28:36 -0400

    added thanks page

---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+/** @type {import('./$types').PageData} */
+// export let data;
+</script>
+
+<div class="bg-black">
+  <!-- top of page -->
+  <div class="h-screen w-3/4 mx-auto text-center py-12 md:py-32 space-y-10">
+    <h1 class="text-5xl font-black">Thank you!!</h1>
+    <h2
+      class="text-3xl md:text-6xl font-black bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text inline-block"
+    >
+      Now check your email, I'm sending you ALL THE GOODIES.
+    </h2>
+  </div>
+</div>
